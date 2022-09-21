35e1e96c14d1baf9abed705d21fb952837164d90
NRI 山方 悠輝 <y-yamagata@nri.co.jp>
2022-09-21 13:18:53 +0900

    Amazon CodeGuru検証用SpringBootアプリのベースプロジェクト作成

---
+++ src/test/java/com/example/codeguru/CodeguruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeguruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
